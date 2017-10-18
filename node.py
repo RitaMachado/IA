275a93686b0d264b27174989225bf9b6d7d402e2
RitaMachado <ritamachado9@hotmail.com>
2017-10-18 18:34:22 +0100

    Revert "graph.pyy"
    This reverts commit dfaa562a4840d87d2463c638dede7d476f762268.

--- node.py
+++ node.py
@@ -4,6 +4,13 @@ Created on Fri Oct 13 14:43:31 2017
 
 @author: ritam
 """
+
+class node:
+
+    def __init__(self, cost=0,state=None,previous=None):
+        self.cost = cost
+        self.state = state  #int? list?
+        self.previous = previous
         
 class vertice:  #necessario?
     
@@ -13,39 +20,17 @@ class vertice:  #necessario?
     
 class launch:
     
-    p_in=list()
-    
-    def __init__(self, max_w=0,fixed_c=0,ci=0,p_in):    #current_w tem que tar nos argumentos?
+    def __init__(self, max_w=0,fixed_c=0,ci=0):
         self.max_w =  max_w
         self.fixed_c = fixed_c
-        self.ci = ci
-        self.pieces_in = p_in
-        self.current_w = 0
-    
-    def add_piece(self, pieces, p_id):  #adicionar fixed qnd 1º vez?
-        self.pieces_in.append(p_id)
-        self.current_w += pieces[p_id]
-
-#    def delete_piece(self,p_id,pieces):
-#        self.pieces_in.remove(p_id)
-#        self.current_w -= pieces[p_id]
-#
-
+        self.ci=ci
+        
         
 class problema:
     
-    def __init__(self, cost=0,launches=None,pieces=None,graph=None):
+    def __init__(self, cost=0,state=None,launches=None,pieces=None):
         self.cost = cost
-        self.initial_state = 0  
-        self.goal_state = len(pieces)
-        self.launches = launches
-        self.pieces =pieces
-        self.graph = graph
+        self.initial_state = state  #int? list?
+        self.launches=launches
+        self.pieces=pieces
     
-class state: #node tree
-    
-    def __init__(self, launches=None,previous=None,cost=0,pieces_sent = 0):
-        self.launches = launches
-        self.previous = previous
-        self.cost = cost
-        self.pieces_sent = pieces_sent
